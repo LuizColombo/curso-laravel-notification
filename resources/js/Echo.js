0d67cee1c4431e516f83152d3b6bb7b2ed2e9545
LuizColombo <colombo74744@hotmail.com>
2025-10-14 15:23:48 -0300

    Finalização do Broadcasting e implementação do Sweet Alert

--- resources/js/Echo.js
+++ resources/js/Echo.js
@@ -1,7 +1,24 @@
+import store from "./vuex/store";
+import swal from "sweetalert";
+
+const typeNotifications = {
+    postCommented: "App\\Notifications\\PostCommented",
+};
+
 if (Laravel.user) {
     Echo.private(`App.Models.User.${Laravel.user}`).notification(
         (notification) => {
-            console.log(notification);
+            if (Laravel.user == notification.data.comment.post.user.id) {
+                if (notification.type === typeNotifications.postCommented) {
+                    swal(
+                        "Novo comentário",
+                        `${notification.data.comment.post.title} foi comentado por ${notification.data.comment.user.name}!`,
+                        "info"
+                    );
+
+                    store.commit("ADD_NOTIFICATION", notification);
+                }
+            }
         }
     );
 }
